6ad3764a632ab13be251cf7671875909c284f8dd
Noureddine <nrdnazhr@gmail.com>
2022-11-09 17:54:06 +1100

    Now you can drag me around! Weeeeeee

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,30 @@
+// Main
+
+const videoThumb    = document.querySelector('.heyEveryone');
+
+let offset          = [0,0];
+let dragging        = false;
+
 //
+
+videoThumb.addEventListener('mousedown', (e) => {
+
+    dragging    = true;
+    offset      = [ videoThumb.offsetLeft - e.clientX, videoThumb.offsetTop - e.clientY,];
+
+}, true);
+
+document.addEventListener('mouseup', () => { dragging = false }, true);
+
+document.addEventListener('mousemove', (e) => {
+
+    e.preventDefault();
+
+    if (dragging) {
+
+        videoThumb.style.left = (e.clientX + offset[0]) + 'px';
+        videoThumb.style.top  = (e.clientY + offset[1]) + 'px';
+
+    }
+
+}, true);
